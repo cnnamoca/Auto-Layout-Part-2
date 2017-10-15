8d9bf60108e70fe870ca2b15c16be3323e60afff
Carlo <carlo.namoca@gmail.com>
2017-10-15 14:00:34 -0700

    Initial Commit

---
+++ Auto Layout Part 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Auto Layout Part 2
+//
+//  Created by Carlo Namoca on 2017-10-15.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
